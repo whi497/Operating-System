87121e2ff310f0b8d5ffe2286a7824f09bb12630
leaf <1624745389@qq.com>
2022-03-16 18:23:40 +0800

    modify

--- project2/myOS/dev/uart.c
+++ project2/myOS/dev/uart.c
@@ -5,12 +5,17 @@
 
 /* 向串口输出一个字符
  * 使用封装好的 outb 函数 */
-void uart_put_char(unsigned char ch) {
+void uart_put_char(unsigned char ch) {//done
     /* todo */
+    outb(0x68,ch);
 }
 
 /* 向串口输出一个字符串
  * 此函数接口禁止修改 */
-void uart_put_chars(char *str) {
+void uart_put_chars(char *str) {//done
     /* todo */
+    int i=0;
+    for(;str[i]!='\0';i++){
+        oub(0x68,str[i]);
+    }
 }
