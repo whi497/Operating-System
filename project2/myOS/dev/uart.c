2cb571d0053e646a849b4b428f951899cc32c1ad
leaf <1624745389@qq.com>
2022-03-17 22:50:32 +0800

    uart and vga

--- project2/myOS/dev/uart.c
+++ project2/myOS/dev/uart.c
@@ -5,17 +5,18 @@
 
 /* 向串口输出一个字符
  * 使用封装好的 outb 函数 */
-void uart_put_char(unsigned char ch) {//done
-    /* todo */
-    outb(0x68,ch);
+void uart_put_char(unsigned char ch) {
+    /* todo//done*/
+    if(ch=='\n') outb(0x68,'\r');
+    else outb(0x68,ch);
 }
 
 /* 向串口输出一个字符串
  * 此函数接口禁止修改 */
-void uart_put_chars(char *str) {//done
-    /* todo */
+void uart_put_chars(char *str) {
+    /* todo//done*/
     int i=0;
     for(;str[i]!='\0';i++){
-        oub(0x68,str[i]);
+        uart_put_chars(str[i]);
     }
 }
--- project2/myOS/dev/vga.c
+++ project2/myOS/dev/vga.c
@@ -14,39 +14,90 @@
 
 /* 将光标设定到特定位置
  * 提示：使用 outb */
-void set_cursor_pos(int pos) {
-    /* todo */
+void set_cursor_pos(unsigned short int pos) {
+    /* todo//done */
+    unsigned char line = 0;
+    unsigned char column = 0;
+    line = pos>>8;
+    column = pos;
+    outb(CURSOR_INDEX_PORT,CURSOR_LINE_REG);//写数据
+    outb(CURSOR_DATA_PORT,line);
+    outb(CURSOR_INDEX_PORT,CURSOR_COL_REG);
+    outb(CURSOR_DATA_PORT,column);
 }
 
-/* 读取光标当前所在位置
+/* 获取光标当前所在位置
  * 提示：使用 inb */ 
-int get_cursor_pos(void) {
-    /* todo */
+unsigned short int get_cursor_pos(void) {
+    /* todo//done */
+    unsigned char line = 0;
+    unsigned char column = 0;
+    outb(CURSOR_INDEX_PORT,CURSOR_LINE_REG);//读数据
+    line=inb(CURSOR_DATA_PORT);
+    outb(CURSOR_INDEX_PORT,CURSOR_COL_REG);
+    column=inb(CURSOR_DATA_PORT);
+    int pos = 0;
+    pos = (pos | line)<<8;
+    pos = pos | column;
+    return pos; 
 }
 
 /* 滚屏，vga 屏幕满时使用。丢弃第一行内容，将剩余行整体向上滚动一行
  * 提示：使用指针修改显存 */
 void scroll_screen(void) {
-    /* todo */
+    /* todo//done */
+    unsigned short int* p = VGA_BASE;
+    unsigned short int* q = VGA_BASE+2*VGA_SCREEN_WIDTH;
+    for (int i = 0; i < (VGA_SCREEN_WIDTH)*(VGA_SCREEN_HEIGHT-1); i++) {
+        *p=*q;
+        p++;q++;
+    }
+    q-=80;
+    for(int i=0; i < VGA_SCREEN_WIDTH; i++){//清空最后一行
+        *q=0x0F000F00;
+        q++;
+    }
+    set_cursor_pos(80*24);
 }
 
 /* 向 vga 的特定光标位置 pos 输出一个字符
  * 提示：使用指针修改显存 */
-void put_char2pos(unsigned char c, int color, int pos) {
-    /* todo */
+void put_char2pos(unsigned char c, int color, unsigned short int pos) {
+    /* todo//done */
+    unsigned short int* p=pos*2+VGA_BASE;
+    unsigned short int data=0;
+    if(c=='\n'|c=='\r') {
+        if(pos/80==24) scroll_screen();//光标在最底端换行
+        else set_cursor_pos((pos/80+1)*80);//换行
+    }
+    else{
+        data = data | (color<<8);
+        data = data | c;
+        *p=data;
+        if(pos==80*25-1) scroll_screen();//已写入最后一个位置
+        else set_cursor_pos(pos+1);
+    }
 }
-
 /* ========= 以下函数接口禁止修改 ========= */
 
 /* 清除屏幕上所有字符，并将光标位置重置到顶格
  * 提示：使用指针修改显存 */
 void clear_screen(void) {
-    /* todo */
+    /* todo//done */
+    unsigned short int* p = VGA_BASE;
+    for (int i = 0; i < VGA_SCREEN_WIDTH * VGA_SCREEN_HEIGHT; i++){
+        *p=0x0F000F00;//黑底白标
+        p++;
+    }
+    set_cursor_pos(0);
 }
 
 /* 向 vga 的当前光标位置输出一个字符串，并移动光标位置到串末尾字符的下一位
  * 如果超出了屏幕范围，则需要滚屏
  * 需要能够处理转义字符 \n */
-void append2screen(char *str, int color) { 
-    /* todo */
+void append2screen(char *str, int color) { //换行和滚屏由put_char2pos()内部处理
+    /* todo//done */
+    for (int i = 0;str[i]!='\0';i++){
+        put_char2pos(str[i],color,get_cursor_pos());
+    }
 }
